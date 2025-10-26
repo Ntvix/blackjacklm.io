795772b54fdd52e68e2780ab0d01e09f474ca702
Ntvix / Louis M <ljwmacauley@gmail.com>
2025-10-26 20:32:49 +0000

    Add files via upload

---
+++ js/firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyADXRfxxVKEsQyilJOoZvufhY9ZalzkAxU",
+  authDomain: "realblack-719eb.firebaseapp.com",
+  databaseURL: "https://realblack-719eb-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "realblack-719eb",
+  storageBucket: "realblack-719eb.firebasestorage.app",
+  messagingSenderId: "751398686405",
+  appId: "1:751398686405:web:163527ff4e1286ab916aac",
+  measurementId: "G-FM88MPNBLW"
+};
